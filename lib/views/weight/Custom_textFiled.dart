1d4385cc6ff1a353d64808d2c9fe00b622bf4f12
shehabbebo <shehabbebo27@gmail.com>
2024-11-03 15:35:25 +0200

    create custom textFiled

---
+++ lib/views/weight/Custom_textFiled.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/constant.dart';
+
+class CustomTextfiled extends StatelessWidget {
+  const CustomTextfiled({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Title',
+        hintStyle: TextStyle(color: kPrimaryColor),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([Color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(
+          color: Color ?? Colors.white,
+        ));
+  }
+}
--- lib/views/weight/addnote_buttomSheet.dart
+++ lib/views/weight/addnote_buttomSheet.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/weight/Custom_textFiled.dart';
 
 class addNoteBottomSheet extends StatelessWidget {
   const addNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(height: 32),
+          CustomTextfiled(),
+        ],
+      ),
+    );
   }
 }
