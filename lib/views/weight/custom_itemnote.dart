2dca3252986104eb9e0c2a76959766ca6f47c9f1
shehabbebo <shehabbebo27@gmail.com>
2024-11-10 17:05:30 +0200

    display data part 2

--- lib/views/weight/custom_itemnote.dart
+++ lib/views/weight/custom_itemnote.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
+  const NoteItem({super.key, required this.note});
 
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -18,7 +20,7 @@ class NoteItem extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
         decoration: BoxDecoration(
-          color: Color(0xffFFCC80),
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
@@ -26,12 +28,12 @@ class NoteItem extends StatelessWidget {
           children: [
             ListTile(
               title: Text(
-                'Flutter Tips',
-                style: TextStyle(color: Colors.black, fontSize: 26),
+                note.title,
+                style: const TextStyle(color: Colors.black, fontSize: 26),
               ),
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 16, bottom: 15),
-                child: Text('Build Your Career with Shehab Mohamed ',
+                child: Text(note.subTitle,
                     style: TextStyle(
                         color: Colors.black.withOpacity(.5), fontSize: 18)),
               ),
@@ -45,7 +47,7 @@ class NoteItem extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(right: 24),
-              child: Text('May21,2024',
+              child: Text(note.date,
                   style: TextStyle(
                       fontSize: 16, color: Colors.black.withOpacity(.4))),
             ),
--- lib/views/weight/notes_list_view.dart
+++ lib/views/weight/notes_list_view.dart
@@ -19,7 +19,9 @@ class NotesListView extends StatelessWidget {
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.symmetric(vertical: 4),
-                child: NoteItem(),
+                child: NoteItem(
+                  note: notes[index],
+                ),
               );
             }),
       );
